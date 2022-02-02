1d1f3423e8b022371b98e6f192bdf7cf8daf1b91
Chrissy <chrissyla13@googlemail.com>
2022-02-02 22:33:40 +0100

    ARRAAAAAYYYY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,63 +9,46 @@ import {
   DragDropRankingChoices,
 } from 'survey-angular';
 import { surveyJson } from './../survey';
-import { HttpClient } from '@angular/common/http';
-import { inject } from '@angular/core/testing';
-import { take } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-
-
-
-
-
-
-
+import { addDoc, collection, doc, Firestore, setDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-
-
 export class AppComponent implements OnInit {
-  
-  
   //Lieblingfarbe
   color: string | undefined;
 
   //Geburtstag
-  birthdate: string | undefined
+  birthdate: string | undefined;
 
   //Fehlsichtigkeit
   fehlsichtigkeit: boolean = false;
 
   //Random Farben
-  randomColorF90: string | undefined ;
-  randomColorF51: string | undefined ;
-  randomColorF69: string | undefined ;
-  randomColorF1: string | undefined ;
+  randomColorF90: string | undefined;
+  randomColorF51: string | undefined;
+  randomColorF69: string | undefined;
+  randomColorF1: string | undefined;
 
   //Rankings
-  survey_1: DragDropRankingChoices | undefined;
-  survey_2: DragDropRankingChoices | undefined;
-
- 
+  survey_1: string | undefined;
+  survey_2: string | undefined;
 
   //constructor(private.document:.document) {}
-  constructor(@Inject(DOCUMENT) private document: Document, private httpClient: HttpClient, private Firestore: AngularFirestore) {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private firestore: Firestore,
+  ) {}
 
   //@Inject(DOCUMENT) private httpClient: HttpClient
-
-
-
 
   ngOnInit() {
     StylesManager.applyTheme('modern');
     const survey = new Model(surveyJson);
     SurveyNG.render('surveyContainer', { model: survey });
 
-    
     // Abspeichern der Eingabefelder-Werte sobald sie geändert werden
     survey.onValueChanged.add((survey: any, options: any) => {
       if (options.name == 'color') {
@@ -74,13 +57,15 @@ export class AppComponent implements OnInit {
       if (options.name == 'farbfehlsichtigkeit') {
         this.fehlsichtigkeit = options.value;
       }
-      if(options.name == 'birthdate') {
+      if (options.name == 'birthdate') {
         this.birthdate = options.value;
       }
-      if(options.name == 'survey_1') {
+      if (options.name == 'survey_1') {
         this.survey_1 == options.value;
+        console.log(options.value)
+        console.log(options.onValueChanged)
       }
-      if(options.name == 'survey_2') {
+      if (options.name == 'survey_2') {
         this.survey_2 == options.value;
       }
     });
@@ -110,19 +95,11 @@ export class AppComponent implements OnInit {
       ) {
         survey.currentPage = 'page2';
       }
-      
 
-      if(page.name == 'page3')
-      {
+      if (page.name == 'page3') {
         const Lieblingsfarbe = this.document.getElementById('question.inputID');
       }
-
-    }
-
-   
-    
-
-    );
+    });
 
     // sobald seite gerendert wurde, svg anhand von id suchen und einfärben
     survey.onAfterRenderPage.add((survey: SurveyModel, options: any) => {
@@ -134,59 +111,62 @@ export class AppComponent implements OnInit {
         }
         const test2 = this.document.getElementById('Form1');
         if (test2) {
-          const randomColorF1 = Math.floor(Math.random()*16777215).toString(16);
-          test2.style.fill = '#'+randomColorF1 as string;
-          console.log(randomColorF1);
+          this.randomColorF1 = Math.floor(Math.random() * 16777215).toString(
+            16
+          );
+          test2.style.fill = ('#' + this.randomColorF1) as string;
+          console.log(this.randomColorF1);
         }
         const test3 = this.document.getElementById('Form_90');
         if (test3) {
-          const randomColorF90 = Math.floor(Math.random()*16777215).toString(16);
-          test3.style.fill = '#'+randomColorF90 as string;
-          console.log(randomColorF90);
+          this.randomColorF90 = Math.floor(Math.random() * 16777215).toString(
+            16
+          );
+          test3.style.fill = ('#' + this.randomColorF90) as string;
+          console.log(this.randomColorF90);
         }
         const test4 = this.document.getElementById('Form_69');
         if (test4) {
-          const randomColorF69 = Math.floor(Math.random()*16777215).toString(16);
-          test4.style.fill = '#'+randomColorF69 as string;
-          console.log(randomColorF69);
+          this.randomColorF69 = Math.floor(Math.random() * 16777215).toString(
+            16
+          );
+          test4.style.fill = ('#' + this.randomColorF69) as string;
+          console.log(this.randomColorF69);
         }
         const test5 = this.document.getElementById('Form_51');
         if (test5) {
-          const randomColorF51 = Math.floor(Math.random()*16777215).toString(16);
-          test5.style.fill = '#'+randomColorF51 as string;
-          console.log(randomColorF51);
+          this.randomColorF51 = Math.floor(Math.random() * 16777215).toString(
+            16
+          );
+          test5.style.fill = ('#' + this.randomColorF51) as string;
+          console.log(this.randomColorF51);
         }
         
       }
-     
- 
-        //this.document.post("https://umfrage-technische-visualistik-default-rtdb.europe-west1.firebasedatabase.app/",this.document.getElementById('question.inputID'));
 
-
+      //this.document.post("https://umfrage-technische-visualistik-default-rtdb.europe-west1.firebasedatabase.app/",this.document.getElementById('question.inputID'));
     });
 
-    survey.onComplete.add(()=>this.onSubmit());
-
-    
-
+    survey.onComplete.add(() => this.onSubmit());
   }
 
-  onSubmit(){
-
-    console.log(this.httpClient);
-    
-const Daten = {'Lieblingsfarbe':this.color,
-'Geburtstag':this.birthdate }
-this.Firestore.collection('users').add(Daten);
-
+  onSubmit() {
 
     
+    const Daten = { 
+      Lieblingsfarbe: this.color, 
+      Geburtstag: this.birthdate, 
+      Fehlsichtigkeit: this.fehlsichtigkeit, 
+      Ranking1: this.survey_1, 
+      Ranking2: this.survey_2,
+      RanFarbe90: this.randomColorF90,
+      RanFarbe69: this.randomColorF69,
+      RanFarbe1: this.randomColorF1,
+      RanFarbe51: this.randomColorF51,
+      };
+
+      console.log(Daten);
+    const usersRef = collection(this.firestore, 'users');
+    addDoc(usersRef, Daten);
   }
-  }
-
-
-
- 
-
-  
-
+}
